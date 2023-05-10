225a8eeb7b8d61ec796aa603eb9673bd17426b2c
Cesar4cm22 <131302098+Cesar4cm22@users.noreply.github.com>
2023-05-10 12:59:57 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const puerto = process.env.PRTO || 3000;
+const cors = require('cors');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs. registerPartials(__dirname + '/view/partials',()=>{});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.render('dashboard');
+})
+
+app.get('/categorias', (req,res)=>{
+    res.render('categorias');
+})
+
+app.get('*',(req,res)=>{
+    res.render('404');
+})
+
+app.listen(puerto,() =>{
+    console.log('El servidor esta corriendo en el puerto : ', puerto);
+})
+
